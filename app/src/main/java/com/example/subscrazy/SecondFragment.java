e7e26b4177446ef8ad3b57f130fe733d47f9bf67
Daniel Chu <n4jn4d@gmail.com>
2022-10-20 04:38:37 -0700

    added data save to onclick

--- app/src/main/java/com/example/subscrazy/SecondFragment.java
+++ app/src/main/java/com/example/subscrazy/SecondFragment.java
@@ -5,7 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,8 +18,12 @@ import com.example.subscrazy.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
 
+    private EditText subNameEdt, priceEdt, dateEdt;
+    private Button saveBtn;
+    private DBHandler dbHandler;
+
     private FragmentSecondBinding binding;
-    Spinner spintime;
+    Spinner recurrenceSpinner;
 
 
 
@@ -25,29 +32,55 @@ public class SecondFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
-
         binding = FragmentSecondBinding.inflate(inflater, container, false);
+
         return binding.getRoot();
-
-
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        spintime = getView().findViewById(R.id.spinner_time);
+        dbHandler = new DBHandler(this.getContext());
+
+        recurrenceSpinner = getView().findViewById(R.id.spinner_time);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
                 R.array.timeselectarray, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spintime.setAdapter(adapter);
+        recurrenceSpinner.setAdapter(adapter);
+
+        subNameEdt = getView().findViewById(R.id.editText_name);
+        priceEdt = getView().findViewById(R.id.editText_price);
+//        recurrenceSpinner = getView().findViewById(R.id.spinner_time);
+        dateEdt = getView().findViewById(R.id.editText_date);
+
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+
+                String subName = subNameEdt.getText().toString();
+                String price = priceEdt.getText().toString();
+                String recurrence = recurrenceSpinner.getSelectedItem().toString();
+                String subDate = dateEdt.getText().toString();
+
+                if (subName.isEmpty() || price.isEmpty() || recurrence.isEmpty() || subDate.isEmpty()) {
+                    Toast.makeText(SecondFragment.this.getContext(), "Please fill all fields..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Subscription sub = new Subscription(subName, price, recurrence, subDate, "");
+                dbHandler.addNewSubscription(sub);
+
+                Toast.makeText(SecondFragment.this.getContext(), "Subscription has been added..", Toast.LENGTH_SHORT).show();
+
+                subNameEdt.setText("");
+                priceEdt.setText("");
+                recurrenceSpinner.setAdapter(null);
+                dateEdt.setText("");
+
+//                NavHostFragment.findNavController(SecondFragment.this)
+//                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
         });
     }

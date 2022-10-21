220caf3fe3d3680a99cdb933b74879cd242b91f1
Hardy7dhaliwal <82923789+Hardy7dhaliwal@users.noreply.github.com>
2022-10-20 23:45:31 -0700

    Added date picker

--- app/src/main/java/com/example/subscrazy/SecondFragment.java
+++ app/src/main/java/com/example/subscrazy/SecondFragment.java
@@ -1,11 +1,14 @@
 package com.example.subscrazy;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -15,6 +18,8 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.subscrazy.databinding.FragmentSecondBinding;
+
+import java.util.Calendar;
 
 public class SecondFragment extends Fragment {
 
@@ -39,9 +44,10 @@ public class SecondFragment extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
-        dbHandler = new DBHandler(this.getContext());
+        super.onViewCreated(view, savedInstanceState);
+        Context thisContext = this.getContext();
+        dbHandler = new DBHandler(thisContext);
 
         recurrenceSpinner = getView().findViewById(R.id.spinner_time);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
@@ -53,17 +59,38 @@ public class SecondFragment extends Fragment {
         priceEdt = getView().findViewById(R.id.editText_price);
 //        recurrenceSpinner = getView().findViewById(R.id.spinner_time);
         dateEdt = getView().findViewById(R.id.editText_date);
-
+        dateEdt.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           final Calendar c = Calendar.getInstance();
+                                           int mYear = c.get(Calendar.YEAR); // current year
+                                           int mMonth = c.get(Calendar.MONTH); // current month
+                                           int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                                           // date picker dialog
+                                           DatePickerDialog datePickerDialog = new DatePickerDialog(thisContext,
+                                                   new DatePickerDialog.OnDateSetListener() {
+                                                       @Override
+                                                       public void onDateSet(DatePicker view, int year,
+                                                                             int monthOfYear, int dayOfMonth) {
+                                                           // set day of month , month and year value in the edit text
+                                                           dateEdt.setText(dayOfMonth + "/"
+                                                                   + (monthOfYear + 1) + "/" + year);
+                                                       }
+                                                   },
+                                                   mYear,
+                                                   mMonth,
+                                                   mDay);
+                                           datePickerDialog.show();
+                                       }
+        });
 
         binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String subName = subNameEdt.getText().toString();
                 String price = priceEdt.getText().toString();
                 String recurrence = recurrenceSpinner.getSelectedItem().toString();
                 String subDate = dateEdt.getText().toString();
-
                 if (subName.isEmpty() || price.isEmpty() || recurrence.isEmpty() || subDate.isEmpty()) {
                     Toast.makeText(SecondFragment.this.getContext(), "Please fill all fields..", Toast.LENGTH_SHORT).show();
                     return;

72ad8e4a726f3713d8a18193c2e5252e537f295e
joe <wwwzhzhzhqqq@gmail.com>
2022-10-25 20:35:25 -0700

    sort by time

--- app/src/main/java/com/example/subscrazy/DBHandler.java
+++ app/src/main/java/com/example/subscrazy/DBHandler.java
@@ -63,11 +63,12 @@ public class DBHandler extends SQLiteOpenHelper {
 
         if (cursorSubscriptions.moveToFirst()) {
             do {
-                subscriptionsArrayList.add(new Subscription(cursorSubscriptions.getString(1),
-                        cursorSubscriptions.getString(2),
+                subscriptionsArrayList.add(new Subscription(
+                        cursorSubscriptions.getString(1),
+                        "CAD$ " + cursorSubscriptions.getString(2),
                         cursorSubscriptions.getString(3),
-                        cursorSubscriptions.getString(4),
-                        cursorSubscriptions.getString(5)));
+                        "Due Date: " + cursorSubscriptions.getString(4),
+                        "Notes: " + cursorSubscriptions.getString(5)));
             } while (cursorSubscriptions.moveToNext());
         }
 
